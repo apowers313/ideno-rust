da0959560421b41e8dcca6672bc638d047c5a329
Adam Powers <apowers@ato.ms>
2021-12-12 00:21:38 -0800

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 // jupyter kernelspec install --name=rusty kernelspec
 
 use serde::{Deserialize, Serialize};
-// use serde_json::Result;
 use std::env;
 use std::error::Error;
 use std::fs;
 use tokio::join;
 use zeromq::prelude::*;
-
-// https://github.com/zeromq/zmq.rs/blob/c3f4eb5f4420ac06e76b2e3896e1995a80d1f08d/examples/message_broker.rs
-// zeromq::DealerSocket::new();
-// zeromq::RepSocket::new();
 
 #[derive(Serialize, Deserialize)]
 struct ConnectionSpec {
@@ -53,14 +48,6 @@ async fn main() {
         create_zmq_dealer(String::from("stdin"), &stdin_conn_str),
         create_zmq_reply(String::from("hb"), &hb_conn_str),
     );
-    // let shell_sock = create_zmq_dealer(shell_conn_str);
-
-    // loop {
-    //     let mut repl: String = shell_sock.recv().await?.try_into()?;
-    //     dbg!(&repl);
-    //     repl.push_str(" Reply");
-    //     shell_sock.send(repl.into()).await?;
-    // }
 }
 
 fn read_conn_spec(path: &String) -> ConnectionSpec {
@@ -77,8 +64,6 @@ fn read_conn_spec(path: &String) -> ConnectionSpec {
     return conn_spec;
 }
 
-// async fn create_zmq_dealer(conn_str: String) -> Result<zeromq::DealerSocket, Box<dyn Error>> {
-
 fn create_conn_str(transport: &String, ip: &String, port: &u32) -> String {
     let conn_str: String = format!("{}://{}:{}", transport, ip, port,);
     return conn_str;
@@ -93,12 +78,9 @@ async fn create_zmq_dealer(name: String, conn_str: &String) -> Result<(), Box<dy
 
     // TODO(apowers313) pop this out into it's own function: we need to send on sock, as well as receive
     loop {
-        // let repl: String = sock.recv().await?.try_into()?;
         let msg = sock.recv().await?;
         dbg!(&msg);
         println!("{} got packet!", name);
-        // repl.push_str(" Reply");
-        // sock.send(repl.into()).await?;
     }
 }
 
@@ -124,11 +106,8 @@ async fn create_zmq_reply(name: String, conn_str: &String) -> Result<(), Box<dyn
     sock.bind(&conn_str).await?;
 
     loop {
-        // let repl: String = sock.recv().await?.try_into()?;
         let msg = sock.recv().await?;
         dbg!(&msg);
         println!("{} got packet!", name);
-        // repl.push_str(" Reply");
-        // sock.send(repl.into()).await?;
     }
 }
